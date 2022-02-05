508312a3716bf48a4db4145106101e5d3c84e487
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-02-05 22:46:00 +0100

    Change path

--- src/main/java/com/kbe/calculator/controller/CalculatorController.java
+++ src/main/java/com/kbe/calculator/controller/CalculatorController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/Calculator")
+@RequestMapping("/calculator")
 public class CalculatorController {
 
     @GetMapping("/calculate")
