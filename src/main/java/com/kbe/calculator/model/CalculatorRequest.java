b54f03288a8750f0ad5098265405ae35a9ebd483
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-02-05 13:46:47 +0100

    Remove final keyword from CalculatorRequest

--- src/main/java/com/kbe/calculator/model/CalculatorRequest.java
+++ src/main/java/com/kbe/calculator/model/CalculatorRequest.java
@@ -1,8 +1,8 @@
 package com.kbe.calculator.model;
 
 public class CalculatorRequest {
-    private final int upvotes = 0;
-    private final int downvotes = 0;
+    private int upvotes = 0;
+    private int downvotes = 0;
 
     public int getUpvotes() {
         return upvotes;
