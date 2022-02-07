67c1dc5c3fcf9e73f946b65a8f74378454bc9d5c
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-02-07 18:08:50 +0100

    Add swagger

---
+++ src/main/java/com/kbe/calculator/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.kbe.calculator.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI applicationApi() {
+        return new OpenAPI()
+                .info(new Info().title("Calculator Api")
+                        .description("Provide a list of gif informations")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0.html"))
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("Calculator Api")
+                        .url("https://github.com/Pflanzmann/kbe_application_service"));
+
+    }
+}
