7da8d4be1499ea3847a846cf91dc45ce8e926fbc
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-02-07 01:58:32 +0100

    Add /api/ to all paths

--- src/main/java/com/kbe/calculator/controller/CalculatorController.java
+++ src/main/java/com/kbe/calculator/controller/CalculatorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/calculator")
+@RequestMapping("/api/calculator")
 public class CalculatorController {
 
     VoteRatioCalculator voteRatioCalculator;
